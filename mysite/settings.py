368b42b66c8c0ec0dea1f6dcd67a36fdcda1de71
Andrés Salcedo <134748294+andresjsalcedo@users.noreply.github.com>
2024-09-12 18:49:06 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'solpedcaf',
         'USER': 'root',
-        'PASSWORD': 'rootsena',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
